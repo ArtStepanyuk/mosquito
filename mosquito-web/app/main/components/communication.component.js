82be3ab311645983905bc93f972e2997346dd941
Arthur <artstepsoftserve@gmail.com>
2017-02-25 11:54:38 +0200

    Update communication.component.js

--- mosquito-web/app/main/components/communication.component.js
+++ mosquito-web/app/main/components/communication.component.js
@@ -2,38 +2,38 @@
 //ToDo pretty html
 const template = `
 <div class="container">
-  <div class="row">
-    <div class="col-md-8 col-md-offset-2 form-group">
-       <div class="input-group">
-          <input type="text"
-                 ng-model="$ctrl.message"
-                 class ="form-control">
-          <span class = "input-group-btn">
+   <div class="row">
+      <div class="col-md-8 col-md-offset-2 form-group">
+         <div class="input-group">
+            <input type="text"
+               ng-model="$ctrl.message"
+               class ="form-control">
+            <span class = "input-group-btn">
             <button ng-click="$ctrl.sendMessage()"
-                    class="btn btn-info" type = "button">
-               Send message
+               class="btn btn-info" type = "button">
+            Send message
             </button>
-          </span>
+            </span>
+         </div>
       </div>
-    </div>
-  </div>
-	<div class="row">
-        <div class="col-sm-12" col-md-12 ng-repeat="message in $ctrl.messages track by $index">
-            <div class="card">
-                <div class="message-card">
-                    <div class="media-left">
-                        <img class="media-object img-circle profile-img" src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png">
-                    </div>
-                    <div class="media-body">
-                        <h2 class="media-heading">Anonymous</h2>
-                        <span class="post-date">{{ ::message.postDate | date: 'medium' }}</span>
-                        <div class="message-text">{{ ::message.text }}</div>
-                    </div>
-                </div>
+   </div>
+   <div class="row">
+      <div class="col-sm-12" col-md-12 ng-repeat="message in $ctrl.messages track by $index">
+         <div class="card">
+            <div class="message-card">
+               <div class="media-left">
+                  <img class="media-object img-circle profile-img" src="http://s3.amazonaws.com/37assets/svn/765-default-avatar.png">
+               </div>
+               <div class="media-body">
+                  <h2 class="media-heading">Anonymous</h2>
+                  <span class="post-date">{{ ::message.postDate | date: 'medium' }}</span>
+                  <div class="message-text">{{ ::message.text }}</div>
+               </div>
             </div>
-        </div>
-        </div>
-	</div>
+         </div>
+      </div>
+   </div>
+</div>
 `;
 
 class communicationCtrl {
